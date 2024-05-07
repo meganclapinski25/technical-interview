598f26e1fb26fe399071d1116a0723b315225d7b
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2024-05-07 13:25:11 -0700

    Update custom-prob.py

--- custom-prob.py
+++ custom-prob.py
@@ -7,24 +7,25 @@
 
 def custom_prob(sentence):
     #initializes the count for each vowel in the list
-    counts = [0] * 5
+    counts = [0] * 5 # O(1)
     #iterates over each character in the sentences
-    for char in sentence:
+    for char in sentence:  # O(n)
         #if the character is a vowel, goes through the list and adds the vowel to the list count
-        if char in 'aeiou':
-            if char == 'a':
-                counts[0] += 1
-            elif char == 'e':
-                counts[1] += 1
-            elif char == 'i':
-                counts[2] += 1
-            elif char == 'o':
-                counts[3] += 1
-            elif char == 'u':
-                counts[4] += 1
+        if char in 'aeiou': # O(1)
+            if char == 'a': # O(1)
+                counts[0] += 1 # O(1)
+            elif char == 'e': # O(1)
+                counts[1] += 1 # O(1)
+            elif char == 'i': # O(1)
+                counts[2] += 1 # O(1)
+            elif char == 'o': # O(1)
+                counts[3] += 1 # O(1)
+            elif char == 'u': # O(1)
+                counts[4] += 1 # O(1)
     
 
     return counts
+    #O(n)
 sentence = "I like really really big birds"
 result = custom_prob(sentence)
 print(result)
