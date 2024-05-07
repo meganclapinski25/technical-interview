3f9ae76ea000b025c5ad0368e07c5563bad32311
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2024-05-07 13:34:35 -0700

    Update ugly-nums.py

--- ugly-nums.py
+++ ugly-nums.py
@@ -4,24 +4,24 @@
 class Solution:
     def isUgly(self, n):
         # If n = 0, that means n is not an ugly number
-        if n == 0:
-            return False
+        if n == 0: # O(1)
+            return False # O(1)
 
         # i iterates over the list with for loop
         # i becomes 2 -> 3 -> 5    
-        for i in [2, 3, 5]:
+        for i in [2, 3, 5]: # O(1)
             # While inputted number(n) / i (2, 3, 5) = 0 
             # while (n/i) is divisible (if remainder = 0)
-            while n % i == 0:
+            while n % i == 0 # O(log n)
                 # Divides n by i using floor division operator (//) 
                 # n continues to be divided by i until it is no longer divisible
-                n //= i
+                n //= i # O(log n)
 
         # If n = 1 that means that all prime factors have been divided out
         # leaving only 2, 3, and 5
         # meaning n is an ugly number
-        return n == 1
-
+        return n == 1 # O(1)
+# O(log n)
 # Test cases
 solution = Solution()
 print(solution.isUgly(9))  # Output: True
@@ -31,23 +31,23 @@ print(solution.isUgly(27)) # Output: True
 class Solution:     
     def is_ugly(self, n):
         # If n is a negative int (0, -1...) it isn't an ugly number
-        if n <= 0:
-            return False
+        if n <= 0: # O(1)
+            return False # O(1)
         
         # While n/2 doesn't provide a remainder (even num)
         # Divides n / 2 and assigns that back to n -> 4//=2 n would become 2 -> 2//=2 n would become 1 
-        while n % 2 == 0:
-            n //= 2
+        while n % 2 == 0: # O(log n)
+            n //= 2 # O(log n)
         
         # Same thing but with 3 
-        while n % 3 == 0:
-            n //= 3
+        while n % 3 == 0: # O(log n)
+            n //= 3 # O(log n)
         
         # Same thing but with 5
-        while n % 5 == 0:
-            n //= 5
+        while n % 5 == 0: # O(log n)
+            n //= 5 # O(log n)
                 
-        return n == 1
+        return n == 1 # O(1)
 
 # Test cases
 solution = Solution()
