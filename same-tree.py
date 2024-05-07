d02f241a3760d334fdb12f32fdd4c340ae4031bb
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2024-05-07 13:27:58 -0700

    Update same-tree.py

--- same-tree.py
+++ same-tree.py
@@ -13,24 +13,25 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         #if both trees are empty returns true
         if not p and not q:
-            return True
+            return True  # O(1)
         #cheacks if one tree has ended and the other hasnt.
         #returns a recursive call to check the left and the right subtrees of p and q are the same 
         if p and q and p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right #O(n)
         
-        return False
-    
+        return False  # O(1)
+     #O(n)
 
 def isSameTreeGBT(p: TreeNode, q: TreeNode) -> bool:
     # If both trees are empty, they are the same
     if not p and not q:
-        return True
+        return True # O(1)
     # If one of the trees is empty while the other is not, they are not the same
     if not p or not q:
-        return False
+        return False # O(1)
     # If the values of the current nodes are not equal, they are not the same
     if p.val != q.val:
-        return False
+        return False # O(1)
     # Recursively check if the left and right subtrees are the same
-    return isSameTreeGBT(p.left, q.left) and isSameTreeGBT(p.right, q.right)
+    return isSameTreeGBT(p.left, q.left) and isSameTreeGBT(p.right, q.right) #O(n)
+#O(n)
