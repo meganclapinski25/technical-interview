5bdc6fbcc4e9befff365f3e40f57479231e39861
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2025-05-07 11:51:25 -0700

    Create morethan5.sql

---
+++ sql/morethan5.sql
@@ -0,0 +1,24 @@
+-- Table: Courses
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | student     | varchar |
+-- | class       | varchar |
+-- +-------------+---------+
+-- (student, class) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table indicates the name of a student and the class in which they are enrolled.
+ 
+
+-- Write a solution to find all the classes that have at least five students.
+
+-- Return the result table in any order.
+
+-- Solution -- 
+
+-- Select the class from the courses table
+Select class From Courses
+-- group by the class
+GROUP BY class
+-- use having to count the classes with more or equal to 5 students
+having count(class) >= 5
