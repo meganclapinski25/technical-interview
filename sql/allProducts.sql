3b5cbf4aa2eb01d7051a6b7ee9ad0cf535aba662
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2025-05-14 12:27:51 -0700

    Create allProducts.sql

---
+++ sql/allProducts.sql
@@ -0,0 +1,39 @@
+-- Table: Customer
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | customer_id | int     |
+-- | product_key | int     |
+-- +-------------+---------+
+-- This table may contain duplicates rows. 
+-- customer_id is not NULL.
+-- product_key is a foreign key (reference column) to Product table.
+ 
+
+-- Table: Product
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | product_key | int     |
+-- +-------------+---------+
+-- product_key is the primary key (column with unique values) for this table.
+ 
+
+-- Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+
+-- Return the result table in any order.
+
+-- Solution -- 
+
+-- select customer_id from Customer table,
+select customer_id 
+from Customer
+-- use groupby to get customers that meet a specfic condition (use aggregations)
+group by customer_id 
+-- counts the qnique products a customer bought
+
+having count(distinct product_key)=
+-- subquery = total number of unique products from Product table
+(select count(product_key) from Product)
